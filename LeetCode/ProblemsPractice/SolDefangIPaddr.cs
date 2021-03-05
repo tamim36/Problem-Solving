a320ef9d961252233503b9c16b3c5c960646288d
tamim36 <tamimarefinanik@gmail.com>
2021-03-06 02:07:52 +0600

    solve two problems

---
+++ LeetCode/ProblemsPractice/SolDefangIPaddr.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemsPractice
+{
+    public class SolDefangIPaddr
+    {
+        public string DefangIPaddr(string address)
+        {
+            return address.Replace(".", "[.]");
+        }
+    }
+}
---
+++ LeetCode/ProblemsPractice/SolTwoSum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemsPractice
+{
+    public class SolTwoSum
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int ind, jnd;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int res = (target - nums[i]);
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[j] == res)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
