5d3af7c9f2238ef9155b1ac3ac7e3fba75d199c9
tamim <tamimarefinanik@gmail.com>
2023-07-09 10:20:27 +0600

    solved problems upto july 2023

--- LeetcodeC++/Main/Main.cpp
+++ LeetcodeC++/Main/Main.cpp
@@ -35,26 +35,70 @@ ListNode* buildListNode(vector<int> vec) {
     return head;
 }
 
+vector<vector<int>> parseStringToMatrix(const string& str) {
+    vector<vector<int>> matrix;
+
+    string trimmedStr = str.substr(1, str.length() - 2);
+
+    stringstream ss(trimmedStr);
+    string token;
+
+    while (getline(ss, token, ']'))
+    {
+        vector<int> row;
+        token = token.substr(token.find('[') + 1);
+        stringstream rowStream(token);
+        string element;
+
+        while (getline(rowStream, element, ',')) {
+            row.push_back(stoi(element));
+        }
+
+        matrix.push_back(row);
+    }
+
+    return matrix;
+}
+
+void visualizeMatrix(vector<vector<int>>& grid) {
+    int m = grid.size(), n = grid[0].size();
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++)
+            cout << grid[i][j] << " ";
+
+        cout << endl;
+    }
+    
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
-    vector<int> vect1{ 10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10 };
+    vector<int> vect1{ 4, 4, 4 };
     vector<char> chvect{ 'A','A','A','B','B','B', 'C','C','C', 'D', 'D', 'E' };
     //int a = Solution::maximumTop(vect, 5);
 
     // Binary Tree Generate
     //struct TreeNode* root = new TreeNode(5);
 
-    vector<int> vect2 = { 1,2,3,4,5,6 };
-    vector<string> vs = { "aba","leetcode" , "ee", "a", "abba"};
+    vector<int> vect2 = { 8,4,10,9,11,2,5,1,6,3,7 };
+    vector<string> vs = { "a==b","b!=a", "c==d", "d==e"};
     vector<string> vs1 = { "04:01","06:44" };
 
-    vector<vector<int>> vect2d{
-        {1, 2},
-        {3},
-        {3},
-        {}
+    vector<vector<string>> vectStr2d{
+        {"John","johnsmith@mail.com","john_newyork@mail.com"},
+        {"John","johnsmith@mail.com","john00@mail.com"},
+        {"Mary","mary@mail.com"},
+        {"John","johnnybravo@mail.com"}
     };
+
+    string vect2dStr = "[[0,0]]";
+    string vect2dStr1 = "[[5,5],[5,0],[4,4],[0,3],[1,0]]";
+    vector<vector<int>> vect2d = parseStringToMatrix(vect2dStr);
+    vector<vector<int>> vect2d1 = parseStringToMatrix(vect2dStr1);
+    // visualizeMatrix(vect2d);
+
 
     vector<vector<int>> vect2dT{
         {2}
@@ -81,7 +125,7 @@ int main()
 
     vector<ListNode*> vln = { al, bl, cl };
 
-    auto res = distance(vect1);
+    auto res = countBlackBlocks(3,3, vect2d);
 
     //cout << b << endl;
 }
